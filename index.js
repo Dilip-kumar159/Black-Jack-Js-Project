deca6fce29fd99eba38803327c48c1d024584bf9
Dilip <dilip45377@gmail.com>
2023-05-27 13:54:34 +0530

    add new Card Button

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-let firstCard = generateRandom()
-let secondCard = generateRandom()
-let cards = [firstCard, secondCard]
-let sum = cards[0] + cards[1]
-let isAlive = true
+let cards = []
+let sum = 0
+let isAlive = false
 let hasBlackJack = false
+let player = {
+    name : "Dilip",
+    chips : 160
+}
 let message = ""
 let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el")
+let playerEl = document.getElementById("player-el")
+
+function generateRandom(){
+
+    let randomNum = Math.floor(Math.random() * 13) + 1
+
+    if(randomNum > 10) return 10
+
+    if(randomNum === 1) return 11
+    
+    else return randomNum
+}
 
 function startGame(){
-   renderGame()
+
+    playerEl.textContent = player.name + " : $" + player.chips
+    isAlive = true
+    let firstCard = generateRandom()
+    let secondCard = generateRandom()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
 function renderGame(){
@@ -28,27 +49,18 @@ function renderGame(){
     }
     else if(sum === 21){
         message = "You won Black Jack Game"
+        playerEl.textContent = player.name + " Congrats 🥳 Click Start Game Button to Continue"
         hasBlackJack = true
     }
     else{
         message = "woops!! You're out of the Game"
+        playerEl.textContent = player.name + " Lost ☹️ Click Start Game Button to Continue"
         isAlive = false
     }
 
     messageEl.textContent = message
 }
 
-
-function generateRandom(){
-
-    let randomNum = Math.floor(Math.random() * 13) + 1
-
-    if(randomNum > 10) return 11
-
-    if(randomNum === 1) return 10
-    
-    else return randomNum
-}
 function newCards(){
 
     if(isAlive === true && hasBlackJack === false){
