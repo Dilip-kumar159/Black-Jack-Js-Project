55833e65352113f41021c562a8bad71e4b2c42c0
Dilip <dilip45377@gmail.com>
2023-05-27 13:17:52 +0530

    add renderGame() and modify html button

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-let firstCard = 10
-let secondCard =11
-
-let sum = firstCard + secondCard
+let firstCard = generateRandom()
+let secondCard = generateRandom()
+let cards = [firstCard, secondCard]
+let sum = cards[0] + cards[1]
+let isAlive = true
+let hasBlackJack = false
 let message = ""
 let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
 
 function startGame(){
+   renderGame()
+}
+
+function renderGame(){
+
+    cardsEl.textContent = "Cards : "
+
+    for(let i=0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum : " + sum
+
     if(sum < 21){
         message = "You want to Draw a Card"
-        messageEl.textContent = message
     }
     else if(sum === 21){
         message = "You won Black Jack Game"
-        messageEl.textContent = message
+        hasBlackJack = true
     }
     else{
         message = "woops!! You're out of the Game"
-        messageEl.textContent = message
+        isAlive = false
     }
+
+    messageEl.textContent = message
+}
+
+
+function generateRandom(){
+
+    let randomNum = Math.floor(Math.random() * 13) + 1
+
+    if(randomNum > 10) return 11
+
+    if(randomNum === 1) return 10
     
+    else return randomNum
+}
+function newCards(){
+
+    if(isAlive === true && hasBlackJack === false){
+        let newCard = generateRandom()
+        sum += newCard
+        cards.push(newCard)
+        renderGame()
+    }
 }
